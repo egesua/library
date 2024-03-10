642157785730833baaaa1e9a361b430b631c2e66
egesua <egesu06@gmail.com>
2024-03-10 16:50:24 +0300

    send data into db by fetch

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Container, Paper } from "@mui/material";
+import { Button, Container, Paper } from "@mui/material";
 
 const Student = () => {
 
     const [name, setName] = useState("");
     const [address, setAddress] = useState("");
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const student = {name, address}
+        console.log(student);
+        fetch("http://localhost:8585/student/add", {
+            method:"POST",
+            headers:{ "Content-Type": "application/json" },
+            body: JSON.stringify(student)
+        })
+        .then(() => {
+            console.log("New student added!")
+        }).catch((err) => {console.log(err)})
+    }
 
   return (
     <div>
@@ -20,9 +34,9 @@ const Student = () => {
         <Container>
           <Paper
             elevation={3}
-            sx={{ padding: "50px 20px", width: 600, margin: "10px auto" }}
+            sx={{ padding: "30px 20px", width: 600, margin: "20px auto" }}
           >
-            <h1 style={{color: "blue", position:"relative", }}><b>Add Student</b></h1>
+            <h1 style={{color: "blue"}}><b>Add Student</b></h1>
             <form>
               <TextField
                 id="outlined-basic"
@@ -41,6 +55,7 @@ const Student = () => {
                 value={address}
                 onChange={(e) => setAddress(e.target.value)}
               />
+              <Button variant="contained" color="success" style={{margin: 4}} onClick={handleClick}>Submit</Button>
             </form>
             {name}
             {address}
